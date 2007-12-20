8ccc7fc8fc3dd0b31b9f49ad010256e5dfcc846a
tasuku <tasuku@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-20 06:36:14 +0000

    modified setup.py for darwin
    git-svn-id: http://svn.coderepos.org/share/lang/python/pytc/trunk@3349 d0d07461-0603-4401-acd4-de1884942a52

--- setup.py
+++ setup.py
@@ -5,9 +5,23 @@ from distutils.core import setup, Extension
 if sys.version_info < (2, 3):
   raise Error, "Python 2.3 or later is required"
 
+include_dirs = []
+library_dirs = ['/usr/local/lib']
+
+if sys.platform == 'darwin':
+  # darwin ports
+  include_dirs.append('/opt/local/include')
+  library_dirs.append('/opt/local/lib')
+  # fink
+  include_dirs.append('/sw/include')
+  library_dirs.append('/sw/lib')
+
 ext = Extension('pytc',
                 libraries = ['tokyocabinet', 'z', 'pthread', 'm', 'c'],
-                sources = ['pytc.c'])
+                sources = ['pytc.c'],
+                include_dirs = include_dirs,
+                library_dirs = library_dirs,
+               )
 
 setup(name = 'pytc',
       version = '0.2',
