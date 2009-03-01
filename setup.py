01379e1b72a6bf5c7dfe193f2f0468fb5225dddd
Rasmus Andersson <rasmus@flajm.com>
2009-03-01 15:26:56 +0100

    moved pytc.py into lib directory

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ if sys.version_info < (2, 3):
 include_dirs = []
 library_dirs = ['/usr/local/lib']
 
+# Add search directories for Mac OS X
 if sys.platform == 'darwin':
   # MacPorts
   include_dirs.append('/opt/local/include')
@@ -16,13 +17,21 @@ if sys.platform == 'darwin':
   include_dirs.append('/sw/include')
   library_dirs.append('/sw/lib')
 
+# Undefine macros
+undef_macros=[]
+if '--debug' in sys.argv or '--debug-smisk' in sys.argv:
+  undef_macros=['NDEBUG']
+
+# C extension
 ext = Extension('_pytc',
                 libraries = ['tokyocabinet'],
                 sources = ['src/pytc.c'],
                 include_dirs = include_dirs,
                 library_dirs = library_dirs,
+                undef_macros = undef_macros
                )
 
+# Package
 setup(name = 'pytc',
       version = '0.7',
       description = 'Tokyo Cabinet Python bindings',
@@ -32,5 +41,7 @@ setup(name = 'pytc',
       license='BSD',
       author = 'Tasuku SUENAGA',
       author_email = 'gunyarakun@sourceforge.jp',
-      ext_modules = [ext]
+      ext_modules = [ext],
+      package_dir = {'': 'lib'},
+      packages = ['pytc']
      )
